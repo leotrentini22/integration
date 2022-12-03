d7a2ef11948b729988bfde44e4770a53c9f718ec
leotrentini22 <trentini.leonardo00@gmail.com>
2022-12-03 19:44:57 +0100

    doxygen second try

--- main.cpp
+++ main.cpp
@@ -13,7 +13,13 @@
 // - extend for stranger domains
 // - plot
 
+/*! \file */
+
 /** Implementation of three different methods for the numerical computation of integrals in one or two dimensions
+ * \f[
+ * |I|=\left| \int_{\Omega} f(x,y) dxdy
+ * \right|
+ * \f]
  * Structure:
  * AbstractIntegrator = Abstract class of integrators, with 3 derived classes
  * MidpointIntegrator, TrapezoidalIntegrator, SimpsonIntegrator
