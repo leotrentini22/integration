72e0a9487b4b2922437a760e815aed7ff68c8006
leotrentini22 <trentini.leonardo00@gmail.com>
2022-12-09 13:19:39 +0100

    google tests try

---
+++ Test/test_integrators.cpp
@@ -0,0 +1,19 @@
+//
+// Created by infos on 09/12/2022.
+//
+#include <gtest/gtest.h>
+#include "fparser/fparser.hh"
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include "Function/AbstractFunction.hpp"
+#include "Function/ForderFunction.hpp"
+#include "Function/ParserFunction.hpp"
+#include "Function/CodedFunction.hpp"
+#include "Integrator/AbstractIntegrator.hpp"
+#include "Integrator/MidpointIntegrator.hpp"
+#include "Integrator/TrapezoidalIntegrator.hpp"
+#include "Integrator/SimpsonIntegrator.hpp"
+#include <iostream>
+#include <string>
